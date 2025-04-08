6fce9b83166af5b288f83ba17edcd7def6499f97
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-04-08 13:26:19 -0400

    feat: added flag to scrape port of spain site

--- golang/main.go
+++ golang/main.go
@@ -7,10 +7,13 @@ import (
 	"time"
 )
 
-const sando = "https://www.movietowne.com/cinemas/nowshowing/san-fernando/"
+const (
+	sando = "https://www.movietowne.com/cinemas/nowshowing/san-fernando/"
+	pos   = "https://www.movietowne.com/cinemas/nowshowing/port-of-spain/"
+)
 
-func scrapeAndSave() error {
-	movies, err := scrapeMovies(sando)
+func scrapeAndSave(url string) error {
+	movies, err := scrapeMovies(url)
 	if err != nil {
 		return err
 	}
@@ -38,12 +41,18 @@ func main() {
 	var (
 		force       = flag.Bool("force", false, "force the scraper to run")
 		shouldPrint = flag.Bool("print", true, "print the movies to stdout")
+		posFlag     = flag.Bool("pos", false, "scrape the POS site")
 	)
 	flag.Parse()
 
+	url := sando
+	if *posFlag {
+		url = pos
+	}
+
 	if *force {
 		fmt.Println("Forcing scrape")
-		err := scrapeAndSave()
+		err := scrapeAndSave(url)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -54,7 +63,7 @@ func main() {
 		if err != nil {
 			if os.IsNotExist(err) {
 				fmt.Println("File does not exist, forcing scrape")
-				err := scrapeAndSave()
+				err := scrapeAndSave(url)
 				if err != nil {
 					fmt.Println(err)
 					return
@@ -68,7 +77,7 @@ func main() {
 
 		if data.ModTime().Day() != time.Now().Day() {
 			fmt.Println("Updating movies.json")
-			err := scrapeAndSave()
+			err := scrapeAndSave(url)
 			if err != nil {
 				fmt.Println(err)
 			}
